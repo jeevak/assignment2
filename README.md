902c017209716c4b30a07b1eedbb508efbfc0dc6
Jeevak Kasarkod <jeevak@gmail.com>
2014-06-08 22:42:00 -0700

    Initial check in

---
+++ README.md
@@ -0,0 +1,28 @@
+# [Restaurant App]
+
+This app is my first attempt at implementing JQuery and HTML5 to implement a simple online ordering system.
+
+* Source: [https://github.com/jeevak/assignment2](https://github.com/jeevak/assignment2)
+* Twitter: [@jeevak](http://twitter.com/jeevak)
+
+
+## Quick start
+
+ Clone the git repo — `git clone
+ https://github.com/jeevak/assignment2.git` - and checkout the tagged
+ release you'd like to use.
+
+
+## Features
+
+* Dynamic menu item listing based on the dropdown choice of restaurant
+* Submit button display driven by click of a checkbox item
+* Restaurant and menu contents are powered by [Firebase](http://www.firebase.com/)
+* Final order is stored in Firebase
+
+##TODO
+
+* Login page
+* Use Bootstrap to make it appealing
+* Add images for the menu items
+
---
+++ js/order.js
@@ -0,0 +1,55 @@
+/**
+ * Created by jeevak on 6/8/14.
+ */
+$(document).ready(function () {
+    $("#restaurants").hide();
+    $("#ready").hide();
+    var restaurantRef = new Firebase('https://jeevak-example.firebaseio.com/restaurants');
+    var query = restaurantRef.limit(10);
+    query.on('child_added', function (snapshot) {
+        var value = snapshot.val();
+        if (value != null) {
+            $("#listOfRestaurants").append("<option value= '" + snapshot.name() + "'>" + value.name + "</option>");
+        } else {
+            alert("Firebase repository is empty. Populate it with a list of restaurants and menu items using JSON.")
+        }
+    });
+    $("#restaurants").show();
+
+
+    $("#listOfRestaurants").change(function () {
+        $("#ready").hide();
+        var choice = $("#listOfRestaurants").val();
+        $("#menuitems").hide();
+        $("#menuitems").empty();
+        //var menuURL= "https://jeevak-example.firebaseio.com/restaurants/"+choice+"/menu";
+        var menuRef = new Firebase("https://jeevak-example.firebaseio.com/restaurants/" + choice + "/menu");
+        var query = menuRef.limit(10);
+
+        query.on('child_added', function (snapshot) {
+            var value = snapshot.val();
+            if (value != null) {
+                $("#menuitems").append("<input type=\"checkbox\" value= '" + value.itemName + "'>" + value.itemName + "</input><br>");
+            } else {
+                alert("No menu items for this restaurant. Check later.")
+            }
+        });
+        $("#menuitems").show();
+
+    });
+
+    $('#menuitems').click(function () {
+        $("#ready").show();
+    });
+
+    $('#submit').click(function () {
+        var checkValues = $('input[type=checkbox]:checked').map(function() {
+            return $(this).val();
+        }).get();
+       var orderRef= new Firebase("https://jeevak-example.firebaseio.com/orders");
+       orderRef.push({"user":"default", "restaurant": $("#listOfRestaurants option:selected").text(),"order":checkValues});
+    });
+});
+
+
+
