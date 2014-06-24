7b37217be8fb95f622ea6cc7bbd23cd843f0177d
Jeevak Kasarkod <jeevak@gmail.com>
2014-06-23 21:39:25 -0700

    Added order and history options and the ability to reload the order form.

--- js/order.js
+++ js/order.js
@@ -4,28 +4,55 @@
 
 
 $("#login-form").click(function (e) {
-    e.stopPropagation();
-    $(".login-credentials").show();
+    var username = $('.login-username').val().trim();
+    var passwd = $('.login-password').val().trim();
+
+    if ((username == '') || (passwd == '')) {
+        e.stopPropagation();
+        $(".login-credentials").show();
+    } else {
+        //console.log('Check credentials...');
+        var loginRef = new Firebase('https://jeevak-example.firebaseio.com/users');
+        var query = loginRef.limit(5);
+        query.on('child_added', function (snapshot) {
+            var value = snapshot.val();
+            //console.log("Username: "+ value.username + " and password: "+ value.password);
+            if ((value.username == username)&&(value.password==passwd)) {
+                $(".ch-home").show();
+                $(".ch-home").children("h1").html("Welcome "+username.charAt(0).toUpperCase()+username.slice(1)+" to Foodie.fi" );
+                $(".ch-login").html("<button type=\"button\" class=\"btn btn-primary\">"+username.charAt(0).toUpperCase()+username.slice(1)+"</button>");
+                sessionStorage.setItem("username",username);
+            }
+        });
+    }
+});
+
+$(".ch-order").click(function(e){
     var restaurantRef = new Firebase('https://jeevak-example.firebaseio.com/restaurants');
-    var query = restaurantRef.limit(10);
-    $("#listOfRestaurants").append("<option selected=\"selected\">Select a restaurant</option>")
-    query.on('child_added', function (snapshot) {
-        var value = snapshot.val();
-        if (value != null) {
-            $("#listOfRestaurants").append("<option value= '" + snapshot.name() + "'>" + value.name + "</option>");
-        } else {
-            alert("Firebase repository is empty. Populate it with a list of restaurants and menu items using JSON.")
-        }
-    });
+    var listQuery = restaurantRef.limit(10);
+    if($("#listOfRestaurants").children("option").length==0) {
+        $("#listOfRestaurants").append("<option selected=\"selected\">Select a restaurant</option>")
+        listQuery.on('child_added', function (snapshot) {
+            var value = snapshot.val();
+            if (value != null) {
+                $("#listOfRestaurants").append("<option value= '" + snapshot.name() + "'>" + value.name + "</option>");
+            } else {
+                alert("Firebase repository is empty. Populate it with a list of restaurants and menu items using JSON.")
+            }
+        });
+    }else{
+        $("#listOfRestaurants option:eq(0)").prop("selected", true);
+    }
     $("#restaurants").show();
 });
 
 $("#listOfRestaurants").change(function () {
+    console.log("Tracking the change in the list choice...");
     $("#ready").hide();
     var choice = $("#listOfRestaurants").val();
     $("#menuitems").hide();
     $("#menuitems").empty();
-    //var menuURL= "https://jeevak-example.firebaseio.com/restaurants/"+choice+"/menu";
+
     var menuRef = new Firebase("https://jeevak-example.firebaseio.com/restaurants/" + choice + "/menu");
     var query = menuRef.limit(10);
 
@@ -55,11 +82,23 @@ $('#submit').click(function () {
         return $(this).val();
     }).get();
     var orderRef = new Firebase("https://jeevak-example.firebaseio.com/orders");
-    orderRef.push({"user": "default", "restaurant": $("#listOfRestaurants option:selected").text(), "order": checkValues});
+    orderRef.push({"user": sessionStorage.getItem("username") , "restaurant": $("#listOfRestaurants option:selected").text(), "order": checkValues});
+    $(".modal-title").html("Order Submitted");
+    $(".modal-body").children("p").html("You can now view this order in your order history");
+    $('#ch-modal').modal('show')
+});
+
+$('#ch-modal').on('show.bs.modal', function (e) {
+    // do something...
+    $("#ready").hide();
+    $('#menuitems').hide();
+    $('#restaurants').hide();
 });
 
 $('#cancel').click(function () {
-    location.reload();
+    $("#ready").hide();
+    $('menuitems').hide();
+    $('#restaurants').hide();
 });
 
 
